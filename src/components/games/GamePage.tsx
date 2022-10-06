c9677f420c23ec49e89a7b88795f7b337c719767
Emil Uhlin <emil.h.uhlin@gmail.com>
2022-10-06 17:46:00 +0200

    Feat: game page now requires authentication

--- src/components/games/GamePage.tsx
+++ src/components/games/GamePage.tsx
@@ -1,6 +1,9 @@
+import { withAuthenticationRequired } from "@auth0/auth0-react"
 
-export default function() {
+export function GamePage() {
 
     return <>
     </>
 }
+
+export default withAuthenticationRequired(GamePage)
