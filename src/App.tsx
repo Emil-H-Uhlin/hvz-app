2454008ca2520401cfcce56d2ed6a965a744fd5d
Emil Uhlin <emil.uhlin@se.experis.com>
2022-10-06 11:25:24 +0200

    initial commit with auth0 login

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,46 @@
-import React from 'react';
-import logo from './logo.svg';
+import { Auth0Provider, useAuth0 } from '@auth0/auth0-react';
+import { useEffect } from 'react';
+
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  function LoginButton() {
+    const { loginWithRedirect } = useAuth0()
+    return <button onClick={loginWithRedirect}>Log in with Auth0</button>
+  }
+
+  function LogoutButton() {
+    const { logout } = useAuth0()
+    return <button onClick={() => logout({returnTo: window.location.origin})}>Log out</button>
+  }
+
+  function UserDisplay() {
+    const { user } = useAuth0()
+
+    return user 
+      ? <>
+        <p>Name: {user.name}</p>
+        <p>Email: {user.email}</p>
+        <p>Uid: {user.sub}</p>
+        <LogoutButton/>
+      </>
+      : <>
+        <LoginButton/>
+      </>
+  }
+
+  return <Auth0Provider
+    domain={process.env.REACT_APP_AUTH0_DOMAIN!}
+    clientId={process.env.REACT_APP_AUTH0_CLIENT_ID!}
+    audience={process.env.REACT_APP_AUTH0_AUDIENCE}
+    redirectUri={window.location.origin}
+    scope="openid profile email">
+      
+      <div className="App">
+        <UserDisplay/>
+      </div>
+    
+    </Auth0Provider>
 }
 
 export default App;
