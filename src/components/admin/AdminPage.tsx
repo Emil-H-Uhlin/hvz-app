c37031e4964af246c3efb8ff6c8ccbd2dd8d3efd
Emil Uhlin <emil.uhlin@se.experis.com>
2022-10-06 16:26:30 +0200

    Feat: added redirect from admin page if user is not admin

--- src/components/admin/AdminPage.tsx
+++ src/components/admin/AdminPage.tsx
@@ -1,6 +1,13 @@
+import { withAuthenticationRequired } from "@auth0/auth0-react"
+import { useEffect } from "react"
+import { Navigate } from "react-router-dom"
+import { hvzUser } from "../../App"
 
-export default function() {
+export function AdminPage({ user }: { user: hvzUser | null }) {
 
     return <>
+         { !user?.isAdmin && <Navigate replace to="/"></Navigate> }
     </>
 }
+
+export default withAuthenticationRequired(AdminPage)
