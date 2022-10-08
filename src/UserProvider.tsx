d381692d167e09fcbacbbf9ca3ded6ba19c012d6
Emil Uhlin <emil.h.uhlin@gmail.com>
2022-10-08 15:47:30 +0200

    Fix: provider no longer expects only several children

--- src/UserProvider.tsx
+++ src/UserProvider.tsx
@@ -4,7 +4,7 @@ import {hvzUser} from "./App";
 // @ts-ignore
 export const UserContext = React.createContext()
 
-export default function ({ children }: {children: JSX.Element}) {
+export default function ({ children }: {children: Array<JSX.Element> | JSX.Element }) {
     const [currentUser, setUser] = useState<hvzUser | null>(null)
 
     return <UserContext.Provider value={[currentUser, setUser]}>
