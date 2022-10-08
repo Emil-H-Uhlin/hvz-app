ce48332d298462ebbf351442d16d3cf4d649c777
Emil Uhlin <emil.h.uhlin@gmail.com>
2022-10-08 15:48:19 +0200

    Feat: re-added routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+
 import { Auth0Provider, useAuth0, User } from '@auth0/auth0-react';
 import UserProvider, { UserContext } from './UserProvider'
-import { useState } from 'react';
 
 import './App.css';
 
-import checkIfAdmin from './util/checkAdmin';
 import LoginPage from "./components/login/LoginPage";
+import {AdminPage} from "./components/admin/AdminPage";
 
 function App() {
   return <Auth0Provider
@@ -14,9 +15,13 @@ function App() {
     audience={process.env.REACT_APP_AUTH0_AUDIENCE}
     redirectUri={window.location.origin}
     scope="openid profile email">
-
       <UserProvider>
-        <LoginPage/>
+          <Router>
+              <Routes>
+                  <Route path="/" element={<LoginPage />} />
+                  <Route path="/admin" element={<AdminPage />} />
+              </Routes>
+          </Router>
       </UserProvider>
     </Auth0Provider>
 }
