c4a52ddbcf48aefcb7e7bb75587880233772d2af
Emil Uhlin <emil.h.uhlin@gmail.com>
2022-10-08 17:39:19 +0200

    Feat: created login-component to set user state before reliant components render

--- src/components/login/LoginPage.tsx
+++ src/components/login/LoginPage.tsx
@@ -1,12 +1,10 @@
 import React, { useContext } from "react";
 import { UserContext } from "../../UserProvider";
 import {useAuth0} from "@auth0/auth0-react";
-import checkIfAdmin from "../../util/checkAdmin";
 
 export default function() {
     // @ts-ignore
     const [hvzUser, setUser] = useContext(UserContext)
-    const { user, getAccessTokenSilently } = useAuth0()
 
     function LoginButton() {
         const { loginWithRedirect } = useAuth0()
@@ -19,16 +17,6 @@ export default function() {
             logout({returnTo: window.location.origin})
             setUser(null)
         }}>Log out</button>
-    }
-
-    if (user && !hvzUser) {
-        (async function() {
-            await getAccessTokenSilently().then(token => setUser({
-                ...user,
-                isAdmin: checkIfAdmin(token),
-                token
-            })).catch(e => console.log(e))
-        })()
     }
 
     return <>
