d89523260253544fd745ebd2862cbc512fe80856
Emil Uhlin <emil.h.uhlin@gmail.com>
2022-10-08 15:31:03 +0200

    Feat: cleaned up app

--- src/App.tsx
+++ src/App.tsx
@@ -1,74 +1,20 @@
 import { Auth0Provider, useAuth0, User } from '@auth0/auth0-react';
+import UserProvider, { UserContext } from './UserProvider'
 import { useState } from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 
 import './App.css';
-import AdminPage from './components/admin/AdminPage';
-import GamePage from './components/games/GamePage';
+
 import checkIfAdmin from './util/checkAdmin';
+import LoginPage from "./components/LoginPage";
 
 function App() {
-  
-  const [currentUser, setUser] = useState<hvzUser | null>(null)
-  
-  function LoginButton() {
-    const { loginWithRedirect } = useAuth0()
-    return <button onClick={loginWithRedirect}>Log in with Auth0</button>
-  }
-
-  function LogoutButton() {
-    const { logout } = useAuth0()
-    return <button onClick={() => {
-      logout({returnTo: window.location.origin})
-      setUser(null)
-    }}>Log out</button>
-  }
-
-  function UserDisplay() {
-    const { user, getAccessTokenSilently } = useAuth0()
-    
-    if (user && !currentUser) {
-      (async function() {
-        await getAccessTokenSilently().then(token => setUser({
-          ...user, 
-          isAdmin: checkIfAdmin(token),
-          token
-        })).catch(e => console.log(e))
-      })()
-    }
-    
-    return currentUser 
-      ? <>
-        <p>Name: {currentUser.name}</p>
-        <p>Email: {currentUser.email}</p>
-        <p>Uid: {currentUser.sub}</p>
-        <p>Admin: {currentUser.isAdmin ? "yes" : "no"}</p>
-        <LogoutButton/>
-      </>
-      : <>
-        <LoginButton/>
-      </>
-  }
-
-
   return <Auth0Provider
     domain={process.env.REACT_APP_AUTH0_DOMAIN!}
     clientId={process.env.REACT_APP_AUTH0_CLIENT_ID!}
     audience={process.env.REACT_APP_AUTH0_AUDIENCE}
     redirectUri={window.location.origin}
     scope="openid profile email">
-      
-      <div className="App">
-        <Router>
-          <Routes>
-            <Route path="/"  element={<UserDisplay />}/>
-            <Route path="/admin" element={<AdminPage user={currentUser} />}/>
-            <Route path="/games" element={<GamePage />}/>
-          </Routes>
-        </Router>
 
-      </div>
-    
     </Auth0Provider>
 }
 
