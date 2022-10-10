fd705161d3ad9952e7492beeec1cd802ca46f77d
Emil Uhlin <emil.h.uhlin@gmail.com>
2022-10-10 16:12:44 +0200

    Feat: added simple styling to TitlePage

--- src/components/title/TitlePage.tsx
+++ src/components/title/TitlePage.tsx
@@ -1,6 +1,6 @@
-import React, {useContext} from "react";
-import {Navigate, redirect} from "react-router-dom";
+import {Navigate} from "react-router-dom";
 import {useAuth0} from "@auth0/auth0-react";
+import "../../styles/app.css"
 
 export default function TitlePage() {
     const { isAuthenticated } = useAuth0()
@@ -10,11 +10,11 @@ export default function TitlePage() {
         return <button onClick={loginWithRedirect}>Log in with Auth0</button>
     }
 
-    return <>
+    return <div className="home">
         <h1>HvZ</h1>
         <h3>Humans vz Zombies</h3>
         <hr/>
         { isAuthenticated && <Navigate to={"/home"} replace /> }
         <LoginButton />
-    </>
+    </div>
 }
