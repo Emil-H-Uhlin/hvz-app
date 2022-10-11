7e5b0ca0abe46a6bec952375052bfe220b96435e
Emil Uhlin <emil.h.uhlin@gmail.com>
2022-10-11 13:11:59 +0200

    Feat: added games-component

--- src/components/home/HomePage.tsx
+++ src/components/home/HomePage.tsx
@@ -1,14 +1,12 @@
 import React, {useContext} from "react";
 import {UserContext} from "../../UserProvider";
+import GamesComponent from "./GamesComponent";
 
 export default function HomePage() {
     // @ts-ignore
     const [hvzUser] = useContext(UserContext)
 
-    return hvzUser && <>
-        <h2>Logged in as user:</h2>
-        <p>Name: {hvzUser.name}</p>
-        <p>Email: {hvzUser.email}</p>
-        <p>Admin: {hvzUser.isAdmin ? "yes" : "no"}</p>
+    return <>
+        <GamesComponent />
     </>
 }
