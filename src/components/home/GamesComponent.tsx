a1766daabae15a9b66b0e1d4775c72bc6851918e
Emil Uhlin <emil.h.uhlin@gmail.com>
2022-10-12 11:15:59 +0200

    Feat: added games api call and temp display items

--- src/components/home/GamesComponent.tsx
+++ src/components/home/GamesComponent.tsx
@@ -1,6 +1,29 @@
+import { useContext, useEffect, useState } from "react"
+import { GameModel } from "../../models/GameModel"
+import { UserContext } from "../../UserProvider"
+import GamesListItem from "./GamesListItem"
 
 export default function GamesComponent() {
-    return <>
-        <p>games</p>
+    // @ts-ignore
+    const [hvzUser] = useContext(UserContext)
+    const [games, setGames] = useState<JSX.Element[]>([])
+    
+    useEffect(() => {
+        
+        (async function() {
+            const response = await fetch(`${process.env.REACT_APP_HVZ_API_BASE_URL}/games`, {
+                headers: {
+                    "Content-Type" : "application/json",
+                    "Authorization": `Bearer ${hvzUser.token}`
+                }
+            })
+
+            setGames((await response.json()).map((g: GameModel) => <GamesListItem game={g} key={g.id}/>))
+        })()
+
+    }, [hvzUser])
+    
+    return hvzUser && <>
+        { games }
     </>
 }
