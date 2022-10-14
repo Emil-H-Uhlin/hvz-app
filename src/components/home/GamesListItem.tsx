92f6f60670acb21cebf03da845c4a3313fd50431
Emil Uhlin <emil.h.uhlin@gmail.com>
2022-10-14 10:57:04 +0200

    Feat: added some styling to gameslistitem

--- src/components/home/GamesListItem.tsx
+++ src/components/home/GamesListItem.tsx
@@ -1,7 +1,8 @@
 import { GameModel } from "../../models/GameModel";
+import "./games.sass"
 
 export default function GamesListItem({game} : { game: GameModel }) {
-    return <div>
+    return <div className={"gamesListItem"}>
         <h2>{game.gameName}</h2>
         <p>
             Id={game.id}, Description='{game.description}'
