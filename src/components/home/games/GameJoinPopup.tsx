32ed6dd485c1f2b588443fbc5f589713105a6c5c
Emil Uhlin <emil.h.uhlin@gmail.com>
2022-10-17 10:58:23 +0200

    Feat: added some styling for game-join popup

--- src/components/home/games/GameJoinPopup.tsx
+++ src/components/home/games/GameJoinPopup.tsx
@@ -1,7 +1,8 @@
+import {GameModel} from "../../../models/GameModel"
 import React from 'react'
 import Popup from 'reactjs-popup'
 
-export default function GameJoinPopup() {
+export default function GameJoinPopup({game} : {game: GameModel}) {
 
 	return <Popup trigger={<button>Join Game</button>}
 		modal
@@ -10,8 +11,23 @@ export default function GameJoinPopup() {
 		{ 	
 			// @ts-ignore
 			close => (
-				<div>
-					<button onClick={close}>test</button>
+				<div className="joinPopup">
+					<div className="header">
+						<h1>{game.gameName}</h1>
+						<p>{game.description}</p>
+					</div>
+					<div className="body">
+
+					</div>
+					<div className="actions">
+						<Popup trigger={<button>Join as Human</button>}>
+
+						</Popup>
+						<Popup trigger={<button>Join as Zombie</button>}>
+							
+						</Popup>
+						<button onClick={close}>Cancel</button>
+					</div>
 				</div>
 			)
 	}
