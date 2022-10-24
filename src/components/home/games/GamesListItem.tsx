542249774226b52e934a6d5a6ea75d28171f8692
Emil Uhlin <emil.h.uhlin@gmail.com>
2022-10-24 11:11:13 +0200

    Feat: fetch player of user in joined games

--- src/components/home/games/GamesListItem.tsx
+++ src/components/home/games/GamesListItem.tsx
@@ -3,11 +3,12 @@ import {GameModel, PlayerModel} from "../../../Models"
 
 import React, {useContext} from 'react'
 import GameJoinPopup from './GameJoinPopup'
-
-import {UserContext} from '../../../UserProvider'
-import "./games.sass"
+import Popup from 'reactjs-popup'
+import {getAuthHeaders, UserContext} from "../../../UserProvider"
 
 import {useQuery} from "react-query";
+
+import "./games.sass"
 
 export default function GamesListItem(
     {game, handleGameJoin, joined} : { game: GameModel, handleGameJoin?: (team: string) => void, joined: boolean }) {
@@ -15,6 +16,19 @@ export default function GamesListItem(
     // @ts-ignore
     const [hvzUser] = useContext(UserContext)
 
+    const {data:player} = useQuery<PlayerModel | null>(`player-game${game.id}`, async function() {
+
+        if (!joined) return null;
+
+        const response = await fetch(`${process.env.REACT_APP_HVZ_API_BASE_URL}/games/${game.id}/currentUser/player`, {
+            headers: {
+                "Content-Type": "application/json",
+                ...getAuthHeaders(hvzUser)
+            }
+        })
+
+        return await response.json()
+    })
 
     return <div className={"gamesListItem"}>
         <div>
@@ -24,7 +38,14 @@ export default function GamesListItem(
         <aside>
             { joined 
                 ? <>
-
+                    <Popup trigger={<button>Show bitecode</button>} modal >
+                        <div style={{
+                            backgroundColor: "black",
+                            color: "white"
+                        }}>
+                            <span>{player?.biteCode}</span>
+                        </div>
+                    </Popup>
                 </> 
                 : <>
                     <GameJoinPopup game={game} onPopupClosed={(result: string) => handleGameJoin?.(result)}/>
