0f7037690e205797ee494ef2f0c3e303d444343f
Emil Uhlin <emil.h.uhlin@gmail.com>
2022-11-28 10:37:44 +0100

    fix: set admin requirement to 2 ADMIN_ permissions recent changes removed third ADMIN_ permission

--- src/Utils.ts
+++ src/Utils.ts
@@ -3,5 +3,5 @@ import jwtDecode from 'jwt-decode'
 export function checkIfAdmin(token: string) {
     const decoded: { permissions: [] } = jwtDecode(token)
 
-    return decoded.permissions.filter((it: string) => it.startsWith("ADMIN_")).length === 3
+    return decoded.permissions.filter((it: string) => it.startsWith("ADMIN_")).length === 2
 }
