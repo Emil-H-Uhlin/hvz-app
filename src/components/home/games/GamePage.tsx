fddf83aed6f5f928d380bfa1b8fa9d82b837a7ed
Emil Uhlin <emil.h.uhlin@gmail.com>
2022-11-29 13:55:23 +0100

    feature: added player based content to game page

--- src/components/home/games/GamePage.tsx
+++ src/components/home/games/GamePage.tsx
@@ -1,13 +1,19 @@
 import {useQuery} from "react-query";
-import {useParams, useNavigate} from 'react-router'
+import {useParams} from 'react-router'
 import {getAuthHeaders, UserContext} from "../../../UserProvider";
-import {useContext} from "react";
+import React, {FormEvent, useContext} from "react";
 import {GameModel, PlayerModel} from "../../../Models";
 
 export default function GamePage() {
     // @ts-ignore
     const hvzUser = useContext(UserContext)
-    const {id} = useParams()    // fetch game id
+    const {id} = useParams()
+
+    function handleBitecodeInput(event: FormEvent<HTMLFormElement>) {
+        console.log(event)
+
+        event.preventDefault()
+    }
 
     function useGameFetch(): [GameModel | undefined, PlayerModel | undefined] {
         const {data: game} = useQuery<GameModel>(`game-${id}`, async function() {
@@ -21,7 +27,6 @@ export default function GamePage() {
             return await response.json()
         }, {
             enabled: hvzUser !== null,
-            retry: 1
         })
 
         const {data:player} = useQuery<PlayerModel>(`player-game${id}`, async function() {
@@ -36,7 +41,6 @@ export default function GamePage() {
             return await response.json()
         }, {
             enabled: hvzUser !== null,
-            retry: 1
         })
 
         return [game, player]
@@ -46,11 +50,37 @@ export default function GamePage() {
 
     return hvzUser && <>
         { !!game
-            ? <div className="gameDisplay">
-                { player && <>member of game</>}
-                <h1>{game.gameName}</h1>
-                <p>{game.description}</p>
-            </div>
+            ? <>
+                <div className="gameDisplay">
+                    { !!player
+                        ? (player.human
+                            ? <div className="bitecode-display">
+                                <img src={"https://api.qrserver.com/v1/create-qr-code/?size=200x200&data="
+                                    + `${process.env.REACT_APP_DOMAIN}/kill?`
+                                    + `bitecode=${player.biteCode}`}/>
+                                <p>{player.biteCode}</p>
+                                <p>Show this to zombies trying to kill you!</p>
+                        </div>
+                            : <div className="zombie-display">
+                                <p>Use your camera app to scan another human-team players bitecode!</p>
+                                <p>Alternatively - manually enter their bitecode: </p>
+                                <form onSubmit={e => handleBitecodeInput(e)}>
+                                    <input type="text"></input>
+                                    <button type="submit">Submit kill</button>
+                                </form>
+                            </div>)
+                        : <>
+                        {/* join game? */}
+                        </> }
+                    <h1>{game.gameName}</h1>
+                    <p>{game.description}</p>
+                    <ul>
+                        <li>Players: {game.playerCount}/{game.maxPlayers}</li>
+                        <li>Game state: {game.gameState}</li>
+                    </ul>
+                </div>
+                {/* map display */}
+            </>
             : <div>error loading game. return to home page?</div>
         }
         </>
