6dd70bd86b4d17c6405c7247070f07d53cd4eb87
Emil Uhlin <emil.h.uhlin@gmail.com>
2022-12-01 10:58:54 +0100

    fix: navigation to app occurs on useeffect rather than render

--- src/components/title/TitlePage.tsx
+++ src/components/title/TitlePage.tsx
@@ -1,10 +1,16 @@
-import {Navigate} from "react-router-dom";
+import {useNavigate} from "react-router";
 import {useAuth0} from "@auth0/auth0-react";
 
 import "./title.sass"
+import {useEffect} from "react";
 
 export default function TitlePage() {
     const { isAuthenticated } = useAuth0()
+    const navigate = useNavigate()
+
+    useEffect(function () {
+        if (isAuthenticated) navigate("/home");
+    }, [isAuthenticated, navigate])
 
     function LoginButton() {
         const { loginWithRedirect } = useAuth0()
@@ -12,8 +18,6 @@ export default function TitlePage() {
     }
 
     return <div className="titlePage">
-        { isAuthenticated && <Navigate to={"/home"} replace /> }
-
         <section>
             <h1>HvZ</h1>
             <h3>Humans vz Zombies</h3>
@@ -29,6 +33,5 @@ export default function TitlePage() {
                 around the world.</cite>
             </p>
         </section>
-
     </div>
 }
