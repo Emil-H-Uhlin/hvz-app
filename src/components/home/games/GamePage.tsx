c3765b9e4bf58168d1dda1c3e3705b77aee09371
Emil <45757491+Emil-H-Uhlin@users.noreply.github.com>
2022-12-01 13:28:57 +0100

    Game features
    * added join game alternatives when not in game
    * updated killpage to use react-router params rather than search
    * feature: added bitecode state to kill-form

--- src/components/home/games/GamePage.tsx
+++ src/components/home/games/GamePage.tsx
@@ -1,9 +1,9 @@
 import {GameModel, jsonToGameModel, PlayerModel} from "../../../Models";
 import {getAuthHeaders, UserContext} from "../../../UserProvider";
 
-import React, {FormEvent, useContext, useEffect} from "react";
+import React, {useContext, useEffect, useState} from "react";
 import {useQuery} from "react-query";
-import {useParams} from 'react-router'
+import {useNavigate, useParams} from 'react-router'
 
 import {MapContainer, TileLayer, Rectangle, useMap} from "react-leaflet";
 import {Map} from 'leaflet'
@@ -14,15 +14,17 @@ export default function GamePage() {
     // @ts-ignore
     const hvzUser = useContext(UserContext)
     const {id} = useParams()
+    const navigate = useNavigate()
 
-    function handleBitecodeInput(event: FormEvent<HTMLFormElement>) {
-        console.log(event)
+    const [biteCodeInput, updateInput] = useState("")
 
+    function handleBitecodeInput(event: any) {
         event.preventDefault()
+        navigate(`/kill/${biteCodeInput}`)
     }
 
-    function useGameFetch(): [GameModel | undefined, PlayerModel | undefined] {
-        const {data: game} = useQuery<GameModel>(`game-${id}`, async function() {
+    function useGameFetch(): [GameModel | undefined, PlayerModel | undefined, () => void] {
+        const {data: game, refetch: refetchGame} = useQuery<GameModel>(`game-${id}`, async function() {
             const response = await fetch(`${process.env.REACT_APP_HVZ_API_BASE_URL}/games/${id}`, {
                 headers: {
                     "Content-Type": "application/json",
@@ -35,7 +37,7 @@ export default function GamePage() {
             enabled: hvzUser !== null,
         })
 
-        const {data:player} = useQuery<PlayerModel>(`player-game${id}`, async function() {
+        const {data:player, refetch: refetchPlayer} = useQuery<PlayerModel>(`player-game${id}`, async function() {
 
             const response = await fetch(`${process.env.REACT_APP_HVZ_API_BASE_URL}/games/${id}/currentUser/player`, {
                 headers: {
@@ -50,10 +52,37 @@ export default function GamePage() {
             retry: 0
         })
 
-        return [game, player]
+        return [game, player, async function() {
+            await refetchGame()
+            await refetchPlayer()
+        }]
     }
 
-    const [game, player] = useGameFetch()
+    const [game, player, refetch] = useGameFetch()
+
+
+    function joinGame(human: boolean) {
+        return fetch(`${process.env.REACT_APP_HVZ_API_BASE_URL}/games/${id}/players`, {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+                ...getAuthHeaders(hvzUser!)
+            },
+            body: JSON.stringify({
+                human
+            })
+        })
+    }
+
+    async function joinAsHuman() {
+        const response = await joinGame(true)
+        if (response.ok) await refetch()
+    }
+
+    async function joinAsZombie() {
+        const response = await joinGame(false)
+        if (response.ok) await refetch()
+    }
 
     return hvzUser && <>
         { !!game
@@ -63,8 +92,8 @@ export default function GamePage() {
                         ? (player.human
                             ? <div className="bitecode-display">
                                 <img src={"https://api.qrserver.com/v1/create-qr-code/?size=200x200&data="
-                                    + `${process.env.REACT_APP_DOMAIN}/kill?`
-                                    + `bitecode=${player.biteCode}`}/>
+                                    + `${process.env.REACT_APP_DOMAIN}/kill/`
+                                    + `${player.biteCode}`}/>
                                 <p>{player.biteCode}</p>
                                 <p>Show this to zombies trying to kill you!</p>
                         </div>
@@ -72,13 +101,15 @@ export default function GamePage() {
                                 <p>Use your camera app to scan another human-team players bitecode!</p>
                                 <p>Alternatively - manually enter their bitecode: </p>
                                 <form onSubmit={e => handleBitecodeInput(e)}>
-                                    <input type="text"></input>
+                                    <input type="text" onChange={e => updateInput(e.target.value)}></input>
                                     <button type="submit">Submit kill</button>
                                 </form>
                             </div>)
-                        : <>
-                        {/* join game? */}
-                        </> }
+                        : <div>
+                            <p>You're not playing this game. Would you like to join it?</p>
+                            <button onClick={joinAsHuman}>Join game (H)</button>
+                            <button onClick={joinAsZombie}>Join game (Z)</button>
+                        </div> }
                     <h1>{game.gameName}</h1>
                     <p>{game.description}</p>
                     <ul>
--- src/components/home/games/KillPage.tsx
+++ src/components/home/games/KillPage.tsx
@@ -1,33 +1,44 @@
 import { withAuthenticationRequired } from "@auth0/auth0-react";
 
-import { useSearchParams, useNavigate } from 'react-router-dom'
-import {useEffect, useContext, useState} from 'react'
-import {useQuery} from 'react-query'
+import { useParams, useNavigate } from 'react-router'
+import {useContext, useEffect, useState} from 'react'
 import {UserContext, getAuthHeaders} from '../../../UserProvider'
 import {KillModel} from "../../../Models"
 
 function KillPage() {
 	// @ts-ignore
-	const {hvzUser} = useContext(UserContext)
+	const hvzUser = useContext(UserContext)
 
-	const [searchParams] = useSearchParams()
+	const { biteCode } = useParams()
 	const navigate = useNavigate()
 
 	const [formState, setFormState] = useState<KillModel>({
-		victimBiteCode: searchParams.get("bitecode")!,
-		story: "",
+		victimBiteCode: "",
 		lat: 0.0,
-		lng: 0.0
+		lng: 0.0,
+		story: ""
 	})
+
+	useEffect(function() {
+		if (biteCode === undefined) navigate("/home");
+
+		setFormState({
+			victimBiteCode: biteCode!,
+			lat: 0.0,
+			lng: 0.0,
+			story: ""
+		})
+
+	}, [biteCode, navigate])
 
 	async function submitKill(event: any) {
 		event.preventDefault()
 
-		const response = await fetch(`${process.env.REACT_APP_HVZ_API_BASE_URL}/kills`, {
+		await fetch(`${process.env.REACT_APP_HVZ_API_BASE_URL}/kills`, {
 			method: "POST",
 			headers: {
 				"Content-Type": "application/json",
-				...getAuthHeaders(hvzUser)
+				...getAuthHeaders(hvzUser!)
 			},
 			body: JSON.stringify(formState)
 		})
@@ -46,23 +57,25 @@ function KillPage() {
 		})
 	}
 
-	return <form onSubmit={submitKill} onChange={handleOnChange}>
-		<fieldset>
-			<label>Story</label>
-			<textarea name="story" />
-		</fieldset>
-		<fieldset>
-			<div>
-				<label>Latitude (x)</label>
-				<input type="number" name="lat" step="0.01" />
-			</div>
-			<div>
-				<label>Longitude (y)</label>
-				<input type="number" name="lng" step="0.01" />
-			</div>
-		</fieldset>
-			<button type="submit">Submit kill</button>
-	</form>
+	return <div>
+		<form onSubmit={submitKill} onChange={handleOnChange}>
+			<fieldset>
+				<label>Story</label>
+				<textarea name="story" />
+			</fieldset>
+			<fieldset>
+				<div>
+					<label>Latitude (x)</label>
+					<input type="number" name="lat" step="0.01" />
+				</div>
+				<div>
+					<label>Longitude (y)</label>
+					<input type="number" name="lng" step="0.01" />
+				</div>
+			</fieldset>
+				<button type="submit">Submit kill</button>
+		</form>
+	</div>
 }
 
 export default withAuthenticationRequired(KillPage)
