fe2f49a11496eeb1001f05afee54256831b35817
Emil <45757491+Emil-H-Uhlin@users.noreply.github.com>
2022-12-02 15:23:35 +0100

    Implemented admin page
    created game edit item which can be used to update a game and update api

--- src/components/admin/AdminPage.tsx
+++ src/components/admin/AdminPage.tsx
@@ -1,19 +1,32 @@
-import { withAuthenticationRequired } from "@auth0/auth0-react"
-import {useContext} from "react";
 import {UserContext} from "../../UserProvider";
-import {Navigate} from "react-router-dom";
+import {GameModel} from "../../Models";
+import GameEditListItem from "./GameEditListItem";
+
+import {useContext, useEffect} from "react";
+import {useQuery} from "react-query";
+import {useNavigate} from "react-router";
+
+import { withAuthenticationRequired } from "@auth0/auth0-react"
 
 function AdminPage() {
     // @ts-ignore
     const hvzUser = useContext(UserContext)
+    const navigate = useNavigate()
 
-    return !!hvzUser ? <>
-            { hvzUser.isAdmin
-                ? <textarea>{hvzUser.token}</textarea>
-                : <Navigate replace to="/" />
-            }
-        </>
-        : <h1>LOADING</h1>
+    const {data: games} = useQuery<GameModel[]>("allGames")
+
+    useEffect(() => {
+        if (!hvzUser || !hvzUser.isAdmin)
+            navigate("/home")
+
+    }, [hvzUser, games, navigate])
+
+    return hvzUser && <>
+        <div>
+            { games?.map((it: GameModel) => <GameEditListItem game={it} key={it.id}/>) }
+        </div>
+        <textarea value={hvzUser.token} readOnly></textarea>
+    </>
 }
 
 export default withAuthenticationRequired(AdminPage)
